5c7b2d0c8851d5ae98edb212c8bcdea7f8a877cc
Maxim Goryachev <maxto4kacom@gmail.com>
2017-01-16 19:02:43 +0200

    buttons done

--- web/js/main.js
+++ web/js/main.js
@@ -1,26 +1,106 @@
-$("#button1").click(function () {
+$("#button1").on('click', function () {
     $("#text1").addClass("red");
 });
 
-$("#button2").click(function () {
+$("#button2").on('click', function () {
     $("#text2").removeClass("red");
 });
 
-$("#button3").click(function () {
+$("#button3").on('click', function () {
     $("#text3").toggleClass("red", 'switch');
 });
 
-$("#button4").click(function(){
-    alert($("#button4").attr("type"));
-
+$("#button4").on('click', function(){
+    var button4 = $('#button4');
+    alert(button4.attr('id'));
 });
 
-$("#button5").click(function(){
+$("#button5").on('click', function(){
     $("#text5").attr("class", "red");
-
 });
 
-$("#button6").click(function () {
+$("#button6").on('click', function () {
     alert('Alert on click');
 });
+
+$('#button7').on('click', function () {
+    $('this').trigger(alert('Alert on trigger'));
+});
+
+$('#button8').on('click', function () {
+    $(this).clone().appendTo('.clone').append('(cloned)');
+});
+
+$('#button9').on('click', function () {
+    console.log($(this).closest('li'));
+});
+
+$('#button10').on('click', function () {
+
+    var each = $('button').each(function (index, item) {
+        item.append(' ');
+    });
+    console.log(each.text());
+
+});
+
+$('#button11').on('click', function () {
+    console.log($('body').find('#button11'));
+});
+
+$('#button12').on('click', function () {
+    $('#text12').fadeIn();
+});
+
+$('#button13').on('click', function () {
+    $('#text13').fadeOut();
+});
+
+$('#button14').on('click', function () {
+    $('#text14').hide();
+});
+
+$('#button15').on('click', function () {
+    $('#text15').show();
+});
+
+$('#button16').on('click', function () {
+    var dateThis = [
+        $(this).height(),
+        $(this).width(),
+        $(this).offset(),
+        $(this).attributes,
+        $(this).parent(),
+        $(this).prev(),
+        $(this).next(),
+        $(this).text()
+    ];
+
+    console.log(dateThis);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
